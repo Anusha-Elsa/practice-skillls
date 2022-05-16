14079758161c9e8b79139043fea1f437ff593255
Anusha_Elsa <anushauniquersn@gmail.com>
2022-05-16 16:50:23 +0530

    second commit

---
+++ ex_03.js
@@ -0,0 +1,7 @@
+const RCB = "lose"
+const SRH = "lose"
+if (RCB == "win" || SRH == "win") {
+    console.log("chennai enters the qualifier")
+} else {
+    console.log("chennai does not enter the qualifier")
+}
