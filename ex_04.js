8ab26aa6abfc9f9b30e522137aa0d83394ce34ba
Anusha_Elsa <anushauniquersn@gmail.com>
2022-05-16 16:55:57 +0530

    second commit

---
+++ ex_04.js
@@ -0,0 +1,9 @@
+const total_balance = 500;
+let withdraw = 200;
+account_balance = total_balance - withdraw
+if (withdraw < total_balance) {
+    console.log(account_balance)
+}
+else {
+    console.log("low balance")
+}
